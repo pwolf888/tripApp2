9d97b0c1c67db0fd5d47c41c3ef377e5cd671325
JONATHAN TURNBULL <jmturnbu@10-150-45-14.dhcp-m.deakin.edu.au>
2017-04-20 10:31:31 +1000

    first commit to check if it works

---
+++ Week6/Week6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week6
+//
+//  Created by JONATHAN TURNBULL on 20/04/2017.
+//  Copyright © 2017 JONATHAN TURNBULL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
